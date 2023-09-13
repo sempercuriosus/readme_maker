cfb56e04d1db9ffa35d48e9f33458ab1a38d8712
sempercuriosus <vespers-garages.0n@icloud.com>
2023-09-13 08:36:23 -0400

    init

---
+++ index.js
@@ -0,0 +1,49 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+
+/*
+    [Sections to include] 
+        - description
+        - TOC
+        - installation 
+        - usage 
+        - license
+        - how to contribute
+        - testing
+        - questions
+
+    [license needs options AND a badge]
+        - need a list of license options and a badge for the selected one
+        - I am thinking that this should be an array of objects and then each object has a license name, badge containing a location, text about what is covered
+
+
+    [Questions to include]
+        - title
+        - description
+        - how to install
+        - how to use
+        - acknowlegements
+
+    [github username]
+        - this goes under questions 
+        - need the form for what the urls look like 
+    
+    [email address]
+        - include instructions on how to reach you
+        - prompt for re-entry if the form does not match?
+    
+    [readme redirect from TOC to section]
+        - include links when making each section to each question header
+
+*/
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
