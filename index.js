a129fb85cbab60baf6e1866c49251370c94fb5d7
sempercuriosus <vespers-garages.0n@icloud.com>
2023-09-16 16:35:32 -0400

    changed the questions to better suit the inquirer module.

--- index.js
+++ index.js
@@ -1,23 +1,91 @@
 // TODO: Include packages needed for this application
+// Needed Inquirer
+const inq = require("inquirer");
 
-// TODO: Create an array of questions for user input
+// #region Text the user will see
+//
+const initNote = "Markdown Generator Started";
+const endNote = "Markdown Completed";
+
+//
+// #endregion Text the user will see
+
+// #region Questions
+//
 
 /**
- * Is an array of question:answer objects
+ * Is an array of objects containing questions to ask the user about their project to use with the readme generation
  */
 const questions = [
-    { "question": "What is the README Title?", "answer": "" },
-    { "question": "What is the application's purpose? Enter in a brief description.", "answer": "" },
-    { "question": "Does the application have any dependancies?", "answer": "" },
-    { "question": "How can the user install the application?", "answer": "" },
-    { "question": "Are there user configurations?", "answer": "" },
-    { "question": "Who wrote this application?", "answer": "" },
-    { "question": "What is the author's email address?", "answer": "" },
-    { "question": "What is the author's Github username?", "answer": "" },
-    { "question": "How can the author be reached for questions?", "answer": "" },
-    { "question": "What License do you want to use?", "answer": "" },
-    { "question": "How can someone else contribute?", "answer": "" },
+    {
+        type: "input",
+        name: "title",
+        message: "What is the README Title?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "What is the application's purpose? Enter in a brief description."
+    },
+    {
+        type: "input",
+        name: "dependancies",
+        message: "To install dependancies what command can be used?",
+        default: 'npm i',
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "To run tests what command can be used?",
+        default: "npm test",
+    },
+    {
+        type: "input",
+        name: "install",
+        message: "How can the user install the application?",
+    },
+    {
+        type: "input",
+        name: "configs",
+        message: "Are there user configurations?",
+        default: false
+    },
+    {
+        type: "input",
+        name: "author",
+        message: "Who wrote this application?"
+    },
+    {
+        type: "input",
+        name: "author_email",
+        message: "What is the author's email address?"
+    },
+    {
+        type: "input",
+        name: "author_github",
+        message: "What is the author's Github username?"
+    },
+    {
+        type: "input",
+        name: "author_contact",
+        message: "How can the author be reached for questions?"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "What License do you want to use?",
+        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+    },
+    {
+        type: "input",
+        name: "contribute",
+        message: "What needs to be known so someone else contribute?"
+    },
 ];
+
+
+//
+// #endregion Questions
 
 // TODO: Create a function to write README file
 function writeToFile (fileName, data) { }
@@ -25,10 +93,12 @@ function writeToFile (fileName, data) { }
 // TODO: Create a function to initialize app
 function init ()
 {
-    for (let i = 0; i < questions.length; i++)
-    {
-        console.log(questions[i].question, questions[i].answer);
-    }
+    console.log(initNote);
+
+    inq
+        .prompt;
+
+    console.log(endNote);
 }
 
 // Function call to initialize app
