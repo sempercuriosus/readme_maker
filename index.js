39bb9c57fd1765d6ca7f4a3bede7aaed742c0210
sempercuriosus <vespers-garages.0n@icloud.com>
2023-09-16 15:58:02 -0400

    added question:answers and readme outline items

--- index.js
+++ index.js
@@ -2,18 +2,21 @@
 
 // TODO: Create an array of questions for user input
 
+/**
+ * Is an array of question:answer objects
+ */
 const questions = [
-    "What is the README Title?",
-    "What is the application's purpose? Enter in a brief description.",
-    "Does the application have any dependancies?",
-    "How can the user install the application?",
-    "Are there user configurations?",
-    "Who wrote this application?",
-    "What is the author's email address?",
-    "What is the author's Github username?",
-    "How can the author be reached for questions?",
-    "What License do you want to use?",
-    "How can someone else contribute?",
+    { "question": "What is the README Title?", "answer": "" },
+    { "question": "What is the application's purpose? Enter in a brief description.", "answer": "" },
+    { "question": "Does the application have any dependancies?", "answer": "" },
+    { "question": "How can the user install the application?", "answer": "" },
+    { "question": "Are there user configurations?", "answer": "" },
+    { "question": "Who wrote this application?", "answer": "" },
+    { "question": "What is the author's email address?", "answer": "" },
+    { "question": "What is the author's Github username?", "answer": "" },
+    { "question": "How can the author be reached for questions?", "answer": "" },
+    { "question": "What License do you want to use?", "answer": "" },
+    { "question": "How can someone else contribute?", "answer": "" },
 ];
 
 // TODO: Create a function to write README file
@@ -22,10 +25,9 @@ function writeToFile (fileName, data) { }
 // TODO: Create a function to initialize app
 function init ()
 {
-
     for (let i = 0; i < questions.length; i++)
     {
-        console.log(questions[i]);
+        console.log(questions[i].question, questions[i].answer);
     }
 }
 
